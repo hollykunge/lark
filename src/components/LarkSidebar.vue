b8dc6a1bfbb4475c37251a7a785f048dea9087c1
hollykunge <hollykunge@163.com>
2019-01-03 14:54:10 +0800

    调整sidebar的图标

--- src/components/LarkSidebar.vue
+++ src/components/LarkSidebar.vue
@@ -1,52 +1,71 @@
 <template>
-<div>
+  <div>
     <div class="logo-con">
       <router-link to="/">
         <!-- <img v-show="!isCollapse" src="../../../static/images/logo.svg" key="max-logo" />
-        <img v-show="isCollapse" src="../../../static/images/logo-min.svg" key="min-logo" /> -->
+        <img v-show="isCollapse" src="../../../static/images/logo-min.svg" key="min-logo" />-->
       </router-link>
     </div>
-    <el-menu default-active="1-4-1"
-    class="lark-sidebar"
-    @open="handleOpen"
-    @close="handleClose"
-    :collapse="isCollapse"
-    background-color="#002140"
-    text-color="#D3D3D3"
-    active-text-color="#FF8700"
+    <el-menu
+      default-active="1-4-1"
+      class="lark-sidebar"
+      @open="handleOpen"
+      @close="handleClose"
+      :collapse="isCollapse"
+      background-color="#002140"
+      text-color="#D3D3D3"
+      active-text-color="#FF8700"
     >
       <el-menu-item index="2" class="lark-sidebar-item">
-        <i class="el-icon-menu icon-style"></i>
-        <span slot="title">协同研讨</span>
-      </el-menu-item>
-      <el-menu-item index="3" class="lark-sidebar-item">
-        <i class="el-icon-document"></i>
+        <div class="icon-style">
+          <font-awesome-icon icon="home" size="2x"/>
+        </div>
         <span slot="title">主页</span>
       </el-menu-item>
+      <el-menu-item index="3" class="lark-sidebar-item">
+        <div class="icon-style">
+          <font-awesome-icon icon="comment" size="2x"/>
+        </div>
+        <span slot="title">研讨</span>
+      </el-menu-item>
+      <el-menu-item index="3" class="lark-sidebar-item">
+        <div class="icon-style">
+          <font-awesome-icon icon="tasks" size="2x"/>
+        </div>
+        <span slot="title">任务</span>
+      </el-menu-item>
+      <el-menu-item index="3" class="lark-sidebar-item">
+        <div class="icon-style">
+          <font-awesome-icon icon="database" size="2x"/>
+        </div>
+        <span slot="title">数据</span>
+      </el-menu-item>
       <el-menu-item index="4" class="lark-sidebar-item">
-        <i class="el-icon-setting"></i>
+        <div class="icon-style">
+          <font-awesome-icon icon="toolbox" size="2x"/>
+        </div>
         <span slot="title">工具</span>
       </el-menu-item>
     </el-menu>
-</div>
+  </div>
 </template>
 
 <script>
-  export default {
-    data () {
-      return {
-        isCollapse: true
-      }
+export default {
+  data() {
+    return {
+      isCollapse: true
+    };
+  },
+  methods: {
+    handleOpen(key, keyPath) {
+      console.log(key, keyPath);
     },
-    methods: {
-      handleOpen (key, keyPath) {
-        console.log(key, keyPath)
-      },
-      handleClose (key, keyPath) {
-        console.log(key, keyPath)
-      }
+    handleClose(key, keyPath) {
+      console.log(key, keyPath);
     }
   }
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -62,14 +81,14 @@
     width: auto;
   }
 }
-.lark-sidebar{
+.lark-sidebar {
   border-right: solid 0px #e6e6e6 !important;
   height: 100%;
   width: 80px;
   .lark-sidebar-item {
-    .icon-style{
-      width: 36px;
-      height: 36px;
+    height: 64px;
+    .icon-style {
+      padding-left: 3px;
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -5,10 +5,17 @@ import App from './App'
 import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-import '@fortawesome/fontawesome-free/css/all.css'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faCoffee, faHome, faComment, faTasks, faDatabase, faToolbox } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 Vue.config.productionTip = false
 Vue.use(ElementUI)
+library.add(faCoffee, faHome, faComment, faTasks, faDatabase, faToolbox)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+Vue.config.productionTip = false
+// fontawesome.library.add(solid)
+// Vue.component('font-awesome-icon', FontAwesomeIcon)
 // for auto update
 import { checkUpdate } from '@/utils/update.js'
 checkUpdate()
