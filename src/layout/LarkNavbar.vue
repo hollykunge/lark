8619ac4489e8066360939b8ebe3896e51bc3dc34
hollykunge <hollykunge@163.com>
2019-01-04 15:01:15 +0800

    修改导航栏样式

---
+++ src/layout/LarkNavbar.vue
@@ -0,0 +1,52 @@
+<template>
+  <el-row class="lark-navbar" type="flex" justify="end">
+    <el-col :span="10">
+      <lark-search></lark-search>
+    </el-col>
+    <el-col :span="6">
+      <div class="lark-navbar-operation">
+        <el-button id="min" size="mini" icon="el-icon-minus" circle @click="handleMinimize"></el-button>
+        <el-button id="max" size="mini" icon="el-icon-plus" circle></el-button>
+        <el-button id="close" size="mini" type="danger" icon="el-icon-close" circle></el-button>
+      </div>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+import { Shell } from "nw.gui";
+import LarkSearch from "./LarkSearch";
+export default {
+  name: "navbar",
+  components: { Shell, LarkSearch },
+  data() {
+    return {
+      isCollapse: true
+    };
+  },
+  methods: {
+    handleOpen(key, keyPath) {
+      console.log(key, keyPath);
+    },
+    handleClose(key, keyPath) {
+      console.log(key, keyPath);
+    },
+    handleMinimize() {
+      let win = Shell.Window.get();
+      win.minimize();
+    }
+  }
+};
+</script>
+
+<style rel="stylesheet/scss" lang="scss" scoped>
+.lark-navbar {
+  -webkit-app-region: drag;
+  &-operation {
+    float: right;
+    .el-button {
+      -webkit-app-region: no-drag;
+    }
+  }
+}
+</style>
