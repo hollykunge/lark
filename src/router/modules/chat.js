b0b87c4c3d787929b44d758c1edb2b2877523910
dd <hollykunge@163.com>
2019-01-15 21:32:27 +0800

    删除chat中没用的

--- src/router/modules/chat.js
+++ src/router/modules/chat.js
@@ -1,14 +1,4 @@
 const chat = [
-  {
-    path: 'message',
-    name: 'message',
-    component: () => import('@/view/chat/message/index'),
-    children: [{
-      path: 'contact',
-      name: 'contact',
-      component: () => import('@/view/chat/contact/index')
-    }]
-  }
 ]
 
 export default chat
