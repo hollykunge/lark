e38899ffdc208c9069a0d494b876f7485772b574
dd <hollykunge@163.com>
2019-01-27 23:08:19 +0800

    调通chatList

--- src/components/chat/ChatGroupBox.vue
+++ src/components/chat/ChatGroupBox.vue
@@ -2,91 +2,70 @@
   <div class="chat-panel">
     <div class="chat-box-list">
       <div class="search-box">
-        <lark-side-window />
-        <Row type="flex"
-             justify="center">
+        <lark-side-window/>
+        <Row type="flex" justify="center">
           <Col span="20">
-          <Input search
-                 placeholder="搜索"
-                 size="small"
-                 class="search"
-                 @on-focus="showSearch()" />
+            <Input search placeholder="搜索" size="small" class="search" @on-focus="showSearch()"/>
           </Col>
-          <Col span="4"
-               push="1">
-          <Button icon="md-close"
-                  size="small"
-                  v-show="showSearchDiv"
-                  @click="closeSearchDiv()"></Button>
-          <Button icon="md-add"
-                  size="small"
-                  v-show="showAdd"
-                  @click="modalCreateChat = true"></Button>
-          <Modal title="新建研讨"
-                 v-model="modalCreateChat"
-                 :mask-closable="false">
-          </Modal>
+          <Col span="4" push="1">
+            <Button icon="md-close" size="small" v-show="showSearchDiv" @click="closeSearchDiv()"></Button>
+            <Button icon="md-add" size="small" v-show="showAdd" @click="modalCreateChat = true"></Button>
+            <Modal title="新建研讨" v-model="modalCreateChat" :mask-closable="false"></Modal>
           </Col>
         </Row>
       </div>
-      <div class="search-result"
-           v-show="showSearchDiv">
-
-      </div>
+      <div class="search-result" v-show="showSearchDiv"></div>
       <div class="group-box">
         <Tabs size="small">
-          <TabPane label="最 近"
-                   icon="ios-time">
+          <TabPane label="最 近" icon="ios-time">
             <ul class="chat-list">
-              <li class="chat"
-                  v-for="chat in chatList">
-                <a href="javascript:"
-                   @click="showChat(chat)">
-                  <img :src="[host + chat.avatar]">
-                  <b>{{ chat.name }}</b>
-                  <p>{{ chat.name }}</p>
+              <li class="chat-item" v-for="chat in chatList">
+                <a href="javascript:" @click="showChat(chat)">
+                  <div class="avatar">
+                    <img :src="chat.avatar">
+                  </div>
+                  <div class="info">
+                    <h3 class="nickname">
+                      <span>{{ chat.lastword }}</span>
+                    </h3>
+                    <p class="msg">{{ chat.name }}</p>
+                  </div>
                 </a>
               </li>
             </ul>
           </TabPane>
-          <TabPane label="群 组"
-                   icon="ios-people">
+          <TabPane label="群 组" icon="ios-people">
             <ul class="group-list">
-              <li class="group"
-                  v-for="group in groupList">
-                <a href="javascript:"
-                   @click="showChat(group)">
+              <li class="group" v-for="group in groupList">
+                <a href="javascript:" @click="showChat(group)">
                   <img :src="[host + group.avatar]">
                   <b>{{ group.name }}</b>
-                  <p>{{ group.name }}</p>
+                  <p>{{ group.lastword }}</p>
                 </a>
               </li>
             </ul>
           </TabPane>
-          <TabPane label="联系人"
-                   icon="ios-contact">
+          <TabPane label="联系人" icon="ios-contact">
             <ul class="contact-list">
-              <li class="contact"
-                  v-for="contact in contactList">
-                <a href="javascript:"
-                   @click="showChat(contact)">
+              <li class="contact" v-for="contact in contactList">
+                <a href="javascript:" @click="showChat(contact)">
                   <img :src="[host + contact.avatar]">
                   <b>{{ contact.name }}</b>
-                  <p>{{ contact.name }}</p>
+                  <p>{{ contact.lastword }}</p>
                 </a>
               </li>
             </ul>
           </TabPane>
         </Tabs>
-
       </div>
     </div>
     <div class="chat-box">
-      <lark-navbar />
+      <lark-navbar/>
     </div>
   </div>
 </template>
 <script>
+import { mapState, mapGetters, mapMutations, mapActions } from "vuex";
 import LarkNavbar from "@/layout/LarkNavbar";
 import LarkSideWindow from "@/components/header/LarkSideWindow";
 import Search from "@/components/chat/ChatSearch";
@@ -110,33 +89,33 @@ export default {
   computed: {
     //需要展示的研讨
     chatList: {
-      get: function () {
-        return this.$store.state.chatList;
+      get: function() {
+        return this.$store.state.user.chatList;
       },
-      set: function (chatList) {
+      set: function(chatList) {
         this.$store.commit("setChatList", chatList);
       }
     },
     //需要展示的群组
     groupList: {
-      get: function () {
+      get: function() {
         return this.$store.state.groupList;
       },
-      set: function (groupList) {
+      set: function(groupList) {
         this.$store.commit("setGroupList", groupList);
       }
     },
     //需要展示的联系人
     contactList: {
-      get: function () {
+      get: function() {
         return this.$store.state.contactList;
       },
-      set: function (contactList) {
+      set: function(contactList) {
         this.$store.commit("setContactList", contactList);
       }
     }
   },
-  data () {
+  data() {
     return {
       modalCreateChat: false,
       host: conf.getHostUrl(),
@@ -146,34 +125,57 @@ export default {
     };
   },
   methods: {
-    closeSearchDiv: function () {
+    ...mapMutations([
+      //
+    ]),
+    ...mapActions(["getChatList", "hasRead"]),
+    stopLoading(name) {
+      this[name] = false;
+    },
+    closeSearchDiv: function() {
       this.showAdd = true;
       this.showSearchDiv = false;
     },
-    showSearch: function () {
+    showSearch: function() {
       this.showAdd = false;
       this.search = "";
       this.showSearchDiv = true;
     },
     // 打开一个聊天对话框
-    showChat: function (chat) {
+    showChat: function(chat) {
       let self = this;
       let chatList = ChatListUtils.getChatList(self.$store.state.user.id);
       // 删除当前用户已经有的会话
-      let newChatList = chatList.filter(function (element) {
+      let newChatList = chatList.filter(function(element) {
         return String(element.id) !== String(chat.id);
       });
       // 重新添加会话，放到第一个
-      let chatV = new Chat(chat.id, chat.name, conf.getHostUrl() + chat.avatar, 0, '', '', '', MessageTargetType.CHAT_GROUP);
+      let chatV = new Chat(
+        chat.id,
+        chat.name,
+        conf.getHostUrl() + chat.avatar,
+        0,
+        "",
+        "",
+        "",
+        MessageTargetType.CHAT_GROUP
+      );
       newChatList.unshift(chatV);
       // 存储到localStorage 的 chatList
       ChatListUtils.setChatList(self.$store.state.user.id, chatList);
-      this.$store.commit('setChatList', newChatList);
+      this.$store.commit("setChatList", newChatList);
       this.$router.push({
-        path: '/index/chatBox',
+        path: "/index/chatBox",
         query: { chatV: chatV }
       });
     }
+  },
+  mounted: function() {
+    this.listLoading = true;
+    // 请求获取消息列表
+    this.getChatList()
+      .then(() => this.stopLoading("listLoading"))
+      .catch(() => this.stopLoading("listLoading"));
   }
 };
 </script>
@@ -236,86 +238,51 @@ export default {
     color: #aaaaaa;
   }
 
-  li {
+  .chat-list {
+    background: #eceae8;
+  }
+  .chat-item {
+    overflow: hidden;
+    padding: 12px 18px 11px;
+    border-bottom: 1px solid #c8c9ca;
+    cursor: pointer;
     list-style: none;
     position: relative;
-    font-size: 1.2rem;
-    cursor: pointer;
-    font-weight: 200;
-    margin-bottom: 2rem;
-
-    h5 {
-      padding: 0.5rem 0;
-      cursor: pointer;
-      font-size: 1.3rem;
-      font-weight: 200;
-
-      i {
-        vertical-align: baseline;
+    .avatar {
+      float: left;
+      margin-right: 10px;
+      position: relative;
+      .img {
+        display: block;
+        width: 40px;
+        height: 40px;
+        border-radius: 2px;
+        -moz-border-radius: 2px;
+        -webkit-border-radius: 2px;
       }
     }
-
-    img {
-      width: 4.4rem;
-      height: 4.4rem;
-      position: absolute;
-      top: 0.4rem;
-      left: 2rem;
-    }
-
-    b {
-      position: absolute;
-      font-size: 1.3rem;
-      left: 7.5rem;
+    .info {
       overflow: hidden;
-      text-overflow: ellipsis;
-      font-weight: 600;
-      top: 0.6rem;
-    }
-
-    p {
-      position: absolute;
-      left: 7.5rem;
-      bottom: 0.4rem;
-      overflow: hidden;
-      text-overflow: ellipsis;
-      white-space: nowrap;
-      width: 75%;
-      font-size: 1.1rem;
-      color: #aaaaaa;
+      .nickname {
+        font-weight: 400;
+        font-size: 13px;
+        color: rgb(10, 10, 10);
+        line-height: 20px;
+      }
+      .msg {
+        color: #666666;
+        font-size: 13px;
+        width: 100%;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+        word-wrap: normal;
+        height: 1.5em;
+      }
     }
   }
-
-  .chat {
-    height: 5.2rem;
-    position: relative;
-
-    a {
-      display: block;
-      width: 100%;
-      height: 100%;
-      color: #f2f2f2;
-      position: relative;
-
-      i {
-        display: inline-block;
-        width: 1.8rem;
-        height: 1.6rem;
-        background-color: #ff0000;
-        border-radius: 50%;
-        color: #f2f2f2;
-        text-align: center;
-        font-style: normal;
-        position: absolute;
-        left: 1rem;
-        top: 0;
-        z-index: 99999999999;
-      }
-
-      p {
-        width: 12rem;
-      }
-    }
+  .ivu-tabs-bar {
+    margin-bottom: 0px !important;
   }
 }
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,12 @@
 import Vue from "vue";
 import Vuex from "vuex";
 
-Vue.use(Vuex);
-
 // 引入模块
 import app from "./modules/app";
 import user from "./modules/user";
 import chat from "./chat";
 
+Vue.use(Vuex);
 export default new Vuex.Store({
   state: {},
   mutations: {},
